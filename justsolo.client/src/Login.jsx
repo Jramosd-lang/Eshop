e2fd11046d4dd4f254096a3c0db17afaa177f60a
jr10p9 <chessjr10p9@gmail.com>
2025-06-26 00:23:46 -0500

    <feat> (se añadio boton de iniciar con google y se estilo el Login)

--- justsolo.client/src/Login.jsx
+++ justsolo.client/src/Login.jsx
@@ -87,34 +87,54 @@ function App() {
                 <div id="containerLogin">
                     <section className="containerLogin">
                         <form onSubmit={handleSubmit}>
-                            <h3>Iniciar Sesión</h3>
+                            <h3 className="titleLogin">Iniciar Sesión</h3>
                             <div className="container-group">
                                 <div className="form-group">
-                                    <label htmlFor="email">Correo electrónico</label>
-                                    <input
-                                        type="email"
-                                        id="email"
-                                        name="email"
-                                        placeholder="tú@ejemplo.com"
-                                        required
-                                        autoFocus
-                                        value={email}
-                                        onChange={e => setEmail(e.target.value)}
-                                    />
+                                    <label htmlFor="email">correo electrónico</label>
+                                    <div className="inputGroup">
+                                        <div className="inputSVG">
+                                            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+                                                stroke="#fffafa"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+                                                id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round">
+                                                </g><g id="SVGRepo_iconCarrier"> <path d="M4 7.00005L10.2 11.65C11.2667 12.45 12.7333 12.45 13.8 11.65L20 7"
+                                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <rect x="3" y="5"
+                                                width="18" height="14" rx="2" stroke="currentColor" stroke-width="2" stroke-linecap="round"></rect> </g></svg>
+                                        </div>
+                                        <input
+                                            type="email"
+                                            id="email"
+                                            name="email"
+                                            placeholder="tú@ejemplo.com"
+                                            required
+                                            autoFocus
+                                            value={email}
+                                            onChange={e => setEmail(e.target.value)}
+                                        />
+                                    </div>
                                 </div>
                                 <div className="form-group">
-                                    <label htmlFor="password">Contraseña</label>
-                                    <input
-                                        type="password"
-                                        id="password"
-                                        name="password"
-                                        placeholder="**********"
-                                        required
-                                        value={password}
-                                        onChange={e => setPassword(e.target.value)}
-                                    />
+                                    <label htmlFor="password">contraseña</label>
+                                    <div className="inputGroup">
+                                        <div className="inputSVG">
+                                            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier"
+                                                stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 14.5V16.5M7 10.0288C7.47142 10 8.05259 10 8.8 10H15.2C15.9474 
+                                                10 16.5286 10 17 10.0288M7 10.0288C6.41168 10.0647 5.99429 10.1455 5.63803 10.327C5.07354 10.6146 4.6146 11.0735 4.32698 11.638C4 12.2798 4 13.1198 4 14.8V16.2C4
+                                                17.8802 4 18.7202 4.32698 19.362C4.6146 19.9265 5.07354 20.3854 5.63803 20.673C6.27976 21 7.11984 21 8.8 21H15.2C16.8802 21 17.7202 21 18.362 20.673C18.9265 20.3854
+                                                19.3854 19.9265 19.673 19.362C20 18.7202 20 17.8802 20 16.2V14.8C20 13.1198 20 12.2798 19.673 11.638C19.3854 11.0735 18.9265 10.6146 18.362 10.327C18.0057 10.1455 17.5883
+                                                10.0647 17 10.0288M7 10.0288V8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8V10.0288" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                                        </div>
+                                        <input
+                                            type="password"
+                                            id="password"
+                                            name="password"
+                                            placeholder="**********"
+                                            required
+                                            value={password}
+                                            onChange={e => setPassword(e.target.value)}
+                                        />
+                                    </div>
                                 </div>
-                                <div className="form-group">
+                                <div className="form-checked">
                                     <input
                                         className="check-box"
                                         type="checkbox"
@@ -122,14 +142,29 @@ function App() {
                                         name="remember"
                                     />
                                     <label htmlFor="remember">Recuérdame</label>
+                                    <a href="/forgot-password">¿Olvidaste tu contraseña?</a>
+
                                 </div>
                             </div>
-                            <p className="help-links">
-                                <a href="/forgot-password">¿Olvidaste tu contraseña?</a> |
-                                <a href="/register">Crear cuenta</a>
-                            </p>
-                            {error && <p style={{ color: '#333333' }}>{error}</p>}
-                            <button className="buttonLogin" type="submit">Iniciar Sesión</button>
+                         
+                            <div className= "section-button-session">
+                                <button className="buttonLogin" type="submit">Iniciar Sesión</button>
+                                ó inicia sesion cón
+                                <button className="button-google"> 
+                                <svg>
+                                        <svg width="20" height="20" viewBox="0 0 256 262" xmlns="http://www.w3.org/2000/svg"
+                                            preserveAspectRatio="xMidYMid"><path d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 
+                                            12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027" fill="#4285F4" /><path
+                                            d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 
+                                            31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1" fill="#34A853" /><path d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 
+                                            71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782" fill="#FBBC05" /><path d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 
+                                            165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251" fill="#EB4335" /></svg>
+
+                                </svg>Google</button>
+                                <div className="a-crear-cuenta">
+                                    ¿no tienes cuenta? <a>Crea tu cuenta</a>
+                                </div>
+                            </div>
                         </form>
                     </section>
                 </div>
